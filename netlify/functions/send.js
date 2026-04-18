233d66a24d09d3cca96ec489cb51fd772dbe5cd7
amtngo <amtngo@gmail.com>
2026-04-18 20:49:50 +0900

    Update send.js

--- netlify/functions/send.js
+++ netlify/functions/send.js
@@ -1,6 +1,27 @@
+const { Resend } = require("resend");
+
 exports.handler = async function(event) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "ok" })
-  };
+  try {
+    const { answers } = JSON.parse(event.body);
+
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: "amtngo@naver.com",
+      subject: "테스트 메일",
+      html: `<pre>${JSON.stringify(answers, null, 2)}</pre>`
+    });
+
+    return {
+      statusCode: 200,
+      body: "메일 전송 성공"
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err.message
+    };
+  }
 };
