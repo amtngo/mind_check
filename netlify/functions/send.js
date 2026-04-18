8ecc7e6966097992c3d95bf9d63203d28c1f114d
amtngo <amtngo@gmail.com>
2026-04-18 21:13:15 +0900

    Simplify email HTML content in send.js
    Updated email HTML format to simplify answers display.

--- netlify/functions/send.js
+++ netlify/functions/send.js
@@ -1,9 +1,7 @@
 const { Resend } = require("resend");
 
 exports.handler = async function(event) {
-
   try {
-
     const { code, answers } = JSON.parse(event.body);
 
     const resend = new Resend(process.env.RESEND_API_KEY);
@@ -12,10 +10,7 @@ exports.handler = async function(event) {
       from: "onboarding@resend.dev",
       to: "amtngo@naver.com",
       subject: "설문 결과",
-      html: `
-        <h3>코드: ${code}</h3>
-        <pre>${JSON.stringify(answers, null, 2)}</pre>
-      `
+      html: `<pre>${JSON.stringify(answers)}</pre>`
     });
 
     return {
